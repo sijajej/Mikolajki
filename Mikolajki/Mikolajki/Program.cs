2b547cf24f21ae9bbee8da5d535f55199033e542
sijajej <39956163+sijajej@users.noreply.github.com>
2020-09-11 15:47:51 +0200

    Add files via upload

---
+++ Mikolajki/Mikolajki/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Mikolajki
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var names = new List<string> { "Bulbi", "Wojtas", "Ania", "Olaf", "Buchcik", "Kasia", "Andrzej", "Wiki", "Aniela", "Dynowski" };
+            var input = Console.ReadLine();
+            var a = "";
+            
+            if (!String.IsNullOrWhiteSpace(input))
+            {
+                names.AddRange(input.Split(" "));
+                
+            }
+
+            var iks = names.Count;
+            for (var i = 0; i < iks  ; i++)
+            {
+                var ran = random.Next(names.Count);
+
+                if (i == 0)
+                {
+                    a = names[ran];
+                }
+
+                Console.Write(names[ran] + " -> ");
+                names.Remove(names[ran]);
+            }
+            Console.WriteLine(a);
+        }
+    }
+    
+
+}
